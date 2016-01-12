5267a4553409fd8c336b6a10b838a2987b4847a8
Andrew Osheroff <andrewosh@gmail.com>
2016-01-11 20:12:48 -0500

    First implementation of getDatabase and associated tests

---
+++ index.js
@@ -0,0 +1,43 @@
+var fs = require('fs')
+var path = require('path')
+var mongoose = require('mongoose')
+
+// TODO : are there any schemas that can be validated with mongoose in binder-db?
+
+/**
+ * Creates a connection to the main Binder MongoDB database
+ *
+ * @param {object} opts - options object containing a config specification
+ * @param {function} cb - callback(err, connection)
+ */
+var getDatabase = function (opts, cb) {
+  if (typeof opts === 'function') {
+    cb = opts
+  }
+  var makeConn = function (config) {
+    var url = 'mongodb://' + config.host
+    if (config.port) {
+      url = url + ':' + config.port
+    }
+    var db = mongoose.createConnection(url + '/' + config.db)
+    db.on('error', function (err) {
+      return cb(err)
+    })
+    db.once('open', function () {
+      return cb(null, db)
+    })
+  }
+  var config = opts.config
+  if (!config) {
+    fs.readFile(path.join(__dirname, 'conf', 'main.json'), function (err, text) {
+      if (err) return cb(err)
+      makeConn(JSON.parse(text))
+    })
+  } else {
+    makeConn(config)
+  }
+}
+
+module.exports = {
+  getDatabase: getDatabase
+}
---
+++ test/all.js
@@ -0,0 +1,30 @@
+var assert = require('assert')
+
+describe('getDatabase', function () {
+  beforeEach(function () {
+    exports.getDatabase = null
+  })
+
+  it('should connect to an existing database and return the \'binder\' db', function (done) {
+    var getDatabase = require('../index.js').getDatabase
+    getDatabase(function (err, conn) {
+      if (err) throw err
+      done()
+    })
+  })
+
+  it('should throw an error if the MongoDB connection fails', function (done) {
+    var getDatabase = require('../index.js').getDatabase
+    var config = {
+      host: 'blah',
+      port: 27017,
+      db: 'binder'
+    }
+    var opts = {}
+    opts.config = config
+    getDatabase(opts, function (err, conn) {
+      assert.notEqual(err, undefined)
+      done()
+    })
+  })
+})
